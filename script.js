89708f384375710dc5575575a7c01f163d9acfb4
benne.klingberg@gmail.com <benjamin.klingberg@chasacademy.se>
2025-11-26 13:33:36 +0100

    add firebase configuration and new user button; update styles and scripts for Netlify deployment

--- script.js
+++ script.js
@@ -428,20 +428,20 @@ init();
 
 //MATRIX RAIN
 let rainPaused = false;
-var c = document.getElementById("c");
-var ctx = c.getContext("2d");
+let c = document.getElementById("c");
+let ctx = c.getContext("2d");
 
 c.height = window.innerHeight;
 c.width = window.innerWidth;
 
-var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}`";
+let matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}`";
 matrix = matrix.split("");
 
-var font_size = 10;
-var columns = c.width / font_size;
-var drops = [];
+let font_size = 10;
+let columns = c.width / font_size;
+let drops = [];
 
-for (var x = 0; x < columns; x++) drops[x] = 1;
+for (let x = 0; x < columns; x++) drops[x] = 1;
 
 function draw() {
  if (isPaused || rainPaused) return; // ← istället för paused
@@ -452,8 +452,8 @@ function draw() {
   ctx.fillStyle = "#cd7f32";
   ctx.font = font_size + "px arial";
 
-  for (var i = 0; i < drops.length; i++) {
-    var text = matrix[Math.floor(Math.random() * matrix.length)];
+  for (let i = 0; i < drops.length; i++) {
+    let text = matrix[Math.floor(Math.random() * matrix.length)];
     ctx.fillText(text, i * font_size, drops[i] * font_size);
 
     if (drops[i] * font_size > c.height && Math.random() > 0.975)
@@ -470,5 +470,9 @@ setInterval(draw, 35);
 document.querySelector(".theme-btn").addEventListener("click", function () {
     rainPaused = !rainPaused;
     this.textContent = rainPaused ? "<theme>" : "<no-theme>";
+});
+
+document.getElementById('newuser').addEventListener("click", () => {
+  location.reload();
 });
 
